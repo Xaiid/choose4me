495db62acded1ab1202b2499d88c763ba0f66131
Jeaneth Farmer <xaid27@gmail.com>
2019-12-04 15:33:15 -0600

    Deleted unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-=======
 import Home from './home.js'
 
 ReactDOM.render(
   <Home />,
   document.getElementById('root')
 );
->>>>>>> 190a227... Basic
